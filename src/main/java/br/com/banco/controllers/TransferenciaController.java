b7cbabe19f36c7ac95eaf2ce87871e7c5e328226
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-07-17 19:21:55 -0400

    feat: Created Controller TransferenciaController

---
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -0,0 +1,27 @@
+package br.com.banco.controllers;
+
+import br.com.banco.entities.Transferencia;
+import br.com.banco.services.TransferenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transferencias")
+public class TransferenciaController {
+
+    private final TransferenciaService transferenciaService;
+
+    @Autowired
+    public TransferenciaController(TransferenciaService transferenciaService) {
+        this.transferenciaService = transferenciaService;
+    }
+
+    @GetMapping
+    public List<Transferencia> obterTodasTransferencias() {
+        return transferenciaService.obterTodasTransferencias();
+    }
+}
