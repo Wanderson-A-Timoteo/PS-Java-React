c271dba7900126b36d7ca86764b96fd99543ce24
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-07-17 19:10:23 -0400

    feat: Created Service TransferenciaService

---
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -0,0 +1,23 @@
+package br.com.banco.services;
+
+import br.com.banco.entities.Transferencia;
+import br.com.banco.repositories.TransferenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransferenciaService {
+
+    private final TransferenciaRepository transferenciaRepository;
+
+    @Autowired
+    public TransferenciaService(TransferenciaRepository transferenciaRepository) {
+        this.transferenciaRepository = transferenciaRepository;
+    }
+
+    public List<Transferencia> obterTodasTransferencias() {
+        return transferenciaRepository.findAll();
+    }
+}
