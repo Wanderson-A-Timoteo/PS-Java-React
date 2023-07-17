d76f84bd2b05c15493be52f6fda2b24ecc7942a8
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-07-17 18:26:59 -0400

    feat(Conta): Added annotation for constructor creation

--- src/main/java/br/com/banco/entities/Conta.java
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -1,6 +1,8 @@
 package br.com.banco.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,8 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
